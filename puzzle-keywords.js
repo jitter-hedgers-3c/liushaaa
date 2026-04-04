acb8853fc7bb2ff3bbc1be3e93895b91ec65ba1d
jitter-hedgers-3c <jitter.hedgers_3c@icloud.com>
2026-04-04 23:33:56 +0800

    Add files via upload

---
+++ puzzle-keywords.js
@@ -0,0 +1,15 @@
+const puzzleKeywords = {
+	"焦思雨": "jiaosiyu.html",
+	"刘岚": "liulan.html",
+	"违规电器": "appliance.html",
+	"综合评分": "score.html",
+	"victories": "victories.html",
+	"花圃": "flower.html",
+	"虞美人": "cornpoppy.html",
+	"李建国": "lijianguo123.html",
+	"vitamin营养药": "vitamin.html",
+	"安保部": "safeguards.html",
+	"校医室": "doctor.html",
+	"管理员登陆": "monitor.html",
+	"YQ-083": "victima.html"
+};
---
+++ puzzle-seek.js
@@ -0,0 +1,13 @@
+const puzzleSeek = {
+	"湘水市": "xiangshui.html",
+	"陈敬山": "chenjingshan.html",
+	"张志远": "zhangzhiyuan.html",
+	"周文彬": "zhouwenbin.html",
+	"湘水市基础教育普及工程": "funda-education.html",
+	"阳城大学": "yangchengu.html",
+	"洋栖中学": "yangqi-report.html",
+	"虞美人": "cornpoppy-sci.html",
+	"海洛因": "heroin.html",
+	"临天崖": "lintian.html",
+	"临天崖景区坠崖事件": "drop.html"
+};
